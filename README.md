51188c5c97031d2ae2ea7047ce3d4b7260244078
José Carlos <josecarlos.garciaortega@mail.uca.es>
2016-05-11 15:00:05 +0200

    Patched convergence test

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Solve a system of equations super fast.
 Jacobi's Method written in CUDA for Nvidia users.
 
 # Known bugs
-- There aren't any check for the convergence.
+- There aren't any check for the convergence. (PATCHED 11/05/2016)
--- kernel.cu
+++ kernel.cu
@@ -4,12 +4,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 #define JACOBI_DEBUG 1
+#define TOL 0.0001
+
+__device__ int flag;
 
 __global__ void internal_jacobi_solve(float * a, float * x0, float * b, unsigned int matrixSize) {
 	unsigned int i, j;
-	float sigma = 0;
+	float sigma = 0, newValue;
 
 	i = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -19,11 +22,14 @@ __global__ void internal_jacobi_solve(float * a, float * x0, float * b, unsigned
 		}
 	}
 
-	x0[i] = (b[i] - sigma) / a[i*matrixSize + i];
+	newValue = (b[i] - sigma) / a[i*matrixSize + i];
+
+	if (abs(x0[i] - newValue) > TOL) flag = 0;
+	x0[i] = newValue;
 }
-cudaError_t cuda_jacobi_solve(float ** a, float * x0, float * b, unsigned int matrixSize) {
+cudaError_t cuda_jacobi_solve(float ** a, float * x0, float * b, unsigned int matrixSize, int * iter) {
 	unsigned int i, j;
-	int  blockSize, minGridSize, gridSize;
+	int  blockSize, minGridSize, gridSize, cpuConvergenceTest, k;
 	float *extended_a = 0, *dev_a = 0, *dev_x0 = 0, *dev_b = 0, *dev_old = 0;
 
 	cudaError_t cudaStatus;
@@ -104,12 +110,17 @@ cudaError_t cuda_jacobi_solve(float ** a, float * x0, float * b, unsigned int ma
 		goto Error;
 	}
 
+	cpuConvergenceTest = 0;
+	k = 0;
 	cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, internal_jacobi_solve, 0, matrixSize);
 	gridSize = (matrixSize + blockSize - 1) / blockSize;
 
-	for (i = 0; i < 100; i++) {
+	for (i = 0; i < 100 && !cpuConvergenceTest; i++) {
+		cpuConvergenceTest = 1;
+		cudaMemcpyToSymbol(flag, &cpuConvergenceTest, sizeof(int));
 
-		internal_jacobi_solve << <gridSize, blockSize>> >(dev_a, dev_x0, dev_b, matrixSize);
+
+		internal_jacobi_solve << <gridSize, blockSize >> >(dev_a, dev_x0, dev_b, matrixSize);
 
 		// Check for any errors launching the kernel
 		cudaStatus = cudaGetLastError();
@@ -125,8 +136,14 @@ cudaError_t cuda_jacobi_solve(float ** a, float * x0, float * b, unsigned int ma
 			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching jacobi!\n", cudaStatus);
 			goto Error;
 		}
+
+		cudaMemcpyFromSymbol(&cpuConvergenceTest, flag, sizeof(int));
+
+		k++;
 	}
 
+
+	*iter = k;
 	cudaStatus = cudaMemcpy(x0, dev_x0, matrixSize* sizeof(float), cudaMemcpyDeviceToHost);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
@@ -144,7 +161,8 @@ Error:
 int main()
 {
 	unsigned int matrixSize = 1000, i, j;
-	float *x0,*b, **test;
+	int iter;
+	float *x0, *b, **test;
 	// Allocate memory for CPU.
 	test = (float**)malloc(sizeof(float*)*matrixSize);
 	b = (float*)malloc(sizeof(float)*matrixSize);
@@ -154,12 +172,12 @@ int main()
 
 		// ONLY FOR TESTING PURPOSE.
 		test[i][i] = 2;
-
+		test[i][1] = 2*i;
 		b[i] = 3;
 		x0[i] = 0;
 	}
 
-	cuda_jacobi_solve(test, x0, b, matrixSize);
+	cuda_jacobi_solve(test, x0, b, matrixSize, &iter);
 
 
 	printf("x0=(");
@@ -170,5 +188,5 @@ int main()
 
 	system("pause");
 
-    return 0;
+	return 0;
 }
